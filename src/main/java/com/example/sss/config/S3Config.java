60d99f4b1395b981735e54e8cbceaded88a9dac5
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-02-27 12:43:01 +0900

    feat s3c fix

--- src/main/java/com/example/sss/config/S3Config.java
+++ src/main/java/com/example/sss/config/S3Config.java
@@ -1,33 +1,33 @@
-package com.example.sss.config;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class S3Config {
-
-    @Value("${sc3.access-key}")
-    private String accessKey;
-
-    @Value("${sc3.secret-key}")
-    private String secretKey;
-
-    @Value("${sc3.region}")
-    private String region;
-
-    @Bean
-    public AmazonS3 amazonS3() {
-
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.fromName(region))
-                .withCredentials(new AWSStaticCredentialsProvider(
-                        new BasicAWSCredentials(accessKey, secretKey)
-                ))
-                .build();
-    }
-}
+//package com.example.sss.config;
+//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+//import com.amazonaws.auth.BasicAWSCredentials;
+//import com.amazonaws.regions.Regions;
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class S3Config {
+//
+//    @Value("${sc3.access-key}")
+//    private String accessKey;
+//
+//    @Value("${sc3.secret-key}")
+//    private String secretKey;
+//
+//    @Value("${sc3.region}")
+//    private String region;
+//
+//    @Bean
+//    public AmazonS3 amazonS3() {
+//
+//        return AmazonS3ClientBuilder.standard()
+//                .withRegion(Regions.fromName(region))
+//                .withCredentials(new AWSStaticCredentialsProvider(
+//                        new BasicAWSCredentials(accessKey, secretKey)
+//                ))
+//                .build();
+//    }
+//}
