659da4c422de7d70fde667ff9830560d7669d899
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-02-28 02:13:27 +0900

    feat controller fix

--- src/main/java/com/example/sss/test/TestController.java
+++ src/main/java/com/example/sss/test/TestController.java
@@ -12,34 +12,34 @@ public class TestController {
 
     private final WebClient webClient;
 
-    // WebClient를 생성자로 주입
     public TestController(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.baseUrl("http://127.0.0.1:9000").build();
     }
 
     @PostMapping
     public ResponseEntity<?> testUpload(
+            @RequestParam("option") String option,
             @RequestParam("url") String url,
             @RequestParam("intend") String intend) {
 
-        // 임시로 받은 데이터 로그로 출력
+        System.out.println("Received opt: " + option);
         System.out.println("Received url: " + url);
         System.out.println("Received intend: " + intend);
 
         try {
-            // WebClient를 사용하여 FastAPI 서버에 POST 요청 보내기
+            final String endpoint = "2".equals(option) ? "/api/v1/retouching" : "/api/v1/guide";
+
             String aiResponse = webClient.post()
                     .uri(uriBuilder -> uriBuilder
-                            .path("/api/v1/guide")  // FastAPI 엔드포인트
-                            .queryParam("user_request", intend)  // 쿼리 파라미터로 전달
-                            .queryParam("image_url", url)  // 쿼리 파라미터로 전달
+                            .path(endpoint)
+                            .queryParam("user_request", intend)
+                            .queryParam("image_url", url)
                             .build())
                     .retrieve()
-                    .bodyToMono(String.class)  // 응답을 문자열로 변환
-                    .block();  // 동기 방식으로 응답을 기다림
-            System.out.println(aiResponse);
+                    .bodyToMono(String.class)
+                    .block();
 
-            // FastAPI에서 받은 응답을 그대로 클라이언트에 반환
+            System.out.println(aiResponse);
             return ResponseEntity.status(HttpStatus.OK).body(aiResponse);
 
         } catch (Exception e) {
